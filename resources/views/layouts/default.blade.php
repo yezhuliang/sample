c7c0953467c81454637029739ecbc8504dc673e0
yezhu <1501905755@qq.com>
2018-11-09 06:13:37 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','Sample') - Laravel 新手入门教程</title>
+    <title>@yield('title','Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     @include('layouts._header')
         <div class="container">
             <div class="col-md-offset-1 col-md-10">
+                @include('shared._messages')
                 @yield('content')
                 @include('layouts._footer')
             </div>
