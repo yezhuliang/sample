e6763d15db8a6dba3b0d984fb895a49988359101
yezhu <1501905755@qq.com>
2018-11-10 09:27:41 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,9 +18,17 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+            session()->flash('info', '您已登录，无需再次操作。');
+            return redirect('/');
+        }
+
+        return $next($request);
+
+     /* if (Auth::guard($guard)->check()) {
             return redirect('/home');
         }
 
         return $next($request);
+        */
     }
 }
